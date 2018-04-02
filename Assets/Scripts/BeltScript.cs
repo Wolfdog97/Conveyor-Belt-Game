e49585eb6af77005a4ac0b9447328dbda5ed1aa1
Unknown <tgr235@nyu.edu>
2018-04-02 17:28:22 -0400

    Sushi Belt

---
+++ Assets/Scripts/BeltScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltScript : MonoBehaviour {
+
+    public GameObject belt;
+
+    //struct{
+    //    GameObject sushi;
+    //}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+        if(other.gameObject.tag == "Belt"){
+            //            Instantiate(//new_belt);
+            Instantiate(belt, this.transform.position, Quaternion.identity);
+
+        }
+	}
+}
