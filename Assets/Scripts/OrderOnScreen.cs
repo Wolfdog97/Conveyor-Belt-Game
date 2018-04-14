0c5b507707aa9b943beff6850a39720199f7f893
Unknown <edanuramail@gmail.com>
2018-04-14 17:54:49 -0400

    Order Mockups instantiated

---
+++ Assets/Scripts/OrderOnScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderOnScreen : MonoBehaviour {
+
+	public float timer;
+	public GameObject[] orders;
+	int index;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		timer -= Time.deltaTime;
+
+		if (timer <= 0)
+		{
+			index = Random.Range(0,orders.Length);
+			Instantiate (orders[index]);
+		}
+	}
+}
