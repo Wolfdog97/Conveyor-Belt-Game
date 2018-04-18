d7df7b83337d8ebc280bdc028145515ec7cb1468
Unknown <tgr235@nyu.edu>
2018-04-18 16:42:37 -0400

    Auto stash before merge of "bestbranch" and "origin/master"

---
+++ Assets/SushiScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SushiScore : MonoBehaviour {
+
+    public float score = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+        if(other.tag == "Sushi"){
+            score -= 10f;
+            Destroy(other);
+        }
+        if(other.tag == "Plate"){
+            //plate checker if statement
+            score += 100f;
+            Destroy(other);
+        }
+	}
+}
