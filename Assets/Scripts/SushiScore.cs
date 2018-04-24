206f41929a0c1173096306c61abf78fb02d2427a
Unknown <tgr235@nyu.edu>
2018-04-24 16:46:59 -0400

    Working on score and scoreboard

--- Assets/Scripts/SushiScore.cs
+++ Assets/Scripts/SushiScore.cs
@@ -1,31 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SushiScore : MonoBehaviour {
-
+    
     public float score = 0f;
+    public Text myText;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        myText.text = "$" + score.ToString();
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
-        if(other.tag == "Sushi"){
+        if(other.tag == "Food"){
             score -= 10f;
             Destroy(other);
+            Debug.Log(score);
         }
         if(other.tag == "Plate"){
             //plate checker if statement
+
+
             score += 100f;
             Destroy(other);
+            Debug.Log(score);
         }
 	}
+
+
 }
