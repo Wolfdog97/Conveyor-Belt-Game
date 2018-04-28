578ccb62e0400a3e3193a0f7d293d8a94ed52718
Unknown <edanuramail@gmail.com>
2018-04-28 17:57:07 -0400

    SOme changes with plates

--- Assets/Scripts/PlateCollision.cs
+++ Assets/Scripts/PlateCollision.cs
@@ -2,25 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VR;
+using Valve.VR.InteractionSystem;
 
 public class PlateCollision : MonoBehaviour {
 
 	public Animator plateAnimator;
 	public bool plateCollided;
+	private Hand hand;
 
 	// Use this for initialization
 	void Start () {
-		plateAnimator = this.GetComponent<Animator> ();
-		plateAnimator.SetBool ("plateCollided", true);
+		hand = GetComponent<Hand>();   
+		plateAnimator = GetComponent<Animator> ();
+
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.A)){
+			plateAnimator.SetTrigger ("plateCollision");
+		}
+
 	}
 
-//	void OnCollisionEnter (Collision collision)
-//	{
-//		if (collision.gameObject.)
-//	}
+	void OnCollisionEnter (Collision collision)
+
+	{
+		print ("Colliding");
+		if (hand.controller.GetHairTriggerDown())
+		{
+			print ("triggering");
+			plateAnimator.SetTrigger ("plateCollision");
+		}
+	}
 }
