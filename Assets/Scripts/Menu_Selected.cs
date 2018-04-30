fef0cbbe0a9c1e4575648ef3ee77d1afe2b6292d
Unknown <tgr235@nyu.edu>
2018-04-30 16:48:59 -0400

    Menu Select

---
+++ Assets/Scripts/Menu_Selected.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Selected : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+        if(other.tag == "Sushi_Salmon")
+        {
+            //play scene
+            Debug.Log("SushiChecker works");
+        }
+        else if(other.tag == "Sushi_Egg")
+        {
+            //quit
+            Debug.Log("Other Checking");
+        }
+	}
+}
